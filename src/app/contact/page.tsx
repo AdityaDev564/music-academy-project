36631e4808abd73cb61009ab663350bfab04ad57
Aditya Dev <adityadev564@gmail.com>
2024-04-15 10:25:20 +0530

    initial commit

---
+++ src/app/contact/page.tsx
@@ -0,0 +1,14 @@
+'use client';
+import Image from "next/image";
+import React from "react";
+import { CardBody, CardContainer, CardItem } from "@/components/ui/3d-card";
+import Link from "next/link";
+import courseData from "@/data/music_courses.json";
+
+function page() {
+  return (
+    <div className="min-h-screen bg-black py-12 pt-36
+    ">contact</div>
+  )
+}
+export default page
